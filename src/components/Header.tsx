734c838bf6c936454ca42cd3a76c34c605f4155d
wait4ndbleed <73363131+wait4ndbleed@users.noreply.github.com>
2023-10-06 18:50:44 -0300

    fix: update css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
           <a href="#" aria-label="Home Page"><img src="../assets/images/logo.svg" alt="" /></a>
         </div>
         <div className="text-white font-normal">
-            <ul className="flex gap-16">
+            <ul className="flex gap-10">
                 <li><a href="#" className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white">About</a></li>
                 <li><a href="#" className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white">Careers</a></li>
                 <li><a href="#" className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white">Events</a></li>
@@ -19,7 +19,7 @@ export const Header = () => {
             </ul>
         </div>
       </div>
-      <div className={`${font.className} uppercase text-white text-7xl border-2 border-white w-1/2 p-8 mt-32`}>
+      <div className={`${font.className} uppercase text-white text-7xl border-2 border-white w-[55%] p-8 mt-32`}>
         Immersive experiences that deliver
       </div>
     </header>
