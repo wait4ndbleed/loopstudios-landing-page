c007db354028cb3c6461c8bdc5d8860e972681fe
wait4ndbleed <73363131+wait4ndbleed@users.noreply.github.com>
2023-10-07 13:39:33 -0300

    feat: create home bot responsive

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,69 +5,69 @@ const font = Josefin_Sans({ subsets: ["latin"], weight: ["300"] });
 export const Gallery = () => {
   return (
     <div
-      className={`${font.className} grid lg:grid-cols-3 xl:grid-cols-4 gap-[31px]`}
+      className={`${font.className} grid lg:grid-cols-3 xl:grid-cols-4 w-full gap-[31px]`}
     >
-      <div className="relative group h-[450px]">
+      <div className="relative group h-40 lg:h-[450px]">
         <div
-          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/desktop/image-deep-earth.jpg')] bg-cover h-[450px] flex flex-col justify-end cursor-pointer`}
+          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/mobile/image-deep-earth.jpg')] lg:bg-[url('../assets/images/desktop/image-deep-earth.jpg')] bg-cover h-40 lg:h-[450px] flex flex-col justify-end cursor-pointer`}
         ></div>
-        <div className="relative uppercase px-10 py-8 text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
+        <div className="relative uppercase px-10 py-8 max-w-[150px] text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
           Deep earth
         </div>
       </div>
-      <div className="relative group h-[450px]">
+      <div className="relative group h-40 lg:h-[450px]">
         <div
-          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/desktop/image-night-arcade.jpg')] bg-cover h-[450px] flex flex-col justify-end cursor-pointer`}
+          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/50 bg-[url('../assets/images/mobile/image-night-arcade.jpg')] lg:bg-[url('../assets/images/desktop/image-night-arcade.jpg')] bg-cover h-40 lg:h-[450px] flex flex-col justify-end cursor-pointer`}
         ></div>
-        <div className="relative uppercase px-10 py-8 text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
+        <div className="relative uppercase px-10 py-8 max-w-[150px] text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
           Night arcade
         </div>
       </div>
-      <div className="relative group h-[450px]">
+      <div className="relative group h-40 lg:h-[450px]">
         <div
-          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 after:top-0 after:left-0 bg-[url('../assets/images/desktop/image-soccer-team.jpg')] bg-cover h-[450px] flex flex-col justify-end cursor-pointer`}
+          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/mobile/image-soccer-team.jpg')] lg:bg-[url('../assets/images/desktop/image-soccer-team.jpg')] bg-cover h-40 lg:h-[450px] flex flex-col justify-end cursor-pointer`}
         ></div>
-        <div className="relative uppercase pl-10 py-8 text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
+        <div className="relative uppercase px-10 py-8 max-w-[250px] text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
           Soccer team vr
         </div>
       </div>
-      <div className="relative group h-[450px]">
+      <div className="relative group h-40 lg:h-[450px]">
         <div
-          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/desktop/image-grid.jpg')] bg-cover h-[450px] flex flex-col justify-end cursor-pointer`}
+          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/mobile/image-grid.jpg')] lg:bg-[url('../assets/images/desktop/image-grid.jpg')] bg-cover h-40 lg:h-[450px] flex flex-col justify-end cursor-pointer`}
         ></div>
-        <div className="relative uppercase px-10 py-8 w-0 text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
+        <div className="relative uppercase px-10 py-8 max-w-[150px] text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
           The grid
         </div>
       </div>
-      <div className="relative group h-[450px]">
+      <div className="relative group h-40 lg:h-[450px]">
         <div
-          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/desktop/image-from-above.jpg')] bg-cover h-[450px] flex flex-col justify-end cursor-pointer`}
+          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/mobile/image-from-above.jpg')] lg:bg-[url('../assets/images/desktop/image-from-above.jpg')] bg-cover h-40 lg:h-[450px] flex flex-col justify-end cursor-pointer`}
         ></div>
-        <div className="relative uppercase pl-10 py-8 text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
+        <div className="relative uppercase px-10 py-8 max-w-[250px] text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
           From up above vr
         </div>
       </div>
-      <div className="relative group h-[450px]">
+      <div className="relative group h-40 lg:h-[450px]">
         <div
-          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/desktop/image-pocket-borealis.jpg')] bg-cover h-[450px] flex flex-col justify-end cursor-pointer`}
+          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/mobile/image-pocket-borealis.jpg')] lg:bg-[url('../assets/images/desktop/image-pocket-borealis.jpg')] bg-cover h-40 lg:h-[450px] flex flex-col justify-end cursor-pointer`}
         ></div>
-        <div className="relative uppercase px-10 py-8 text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
+        <div className="relative uppercase px-10 py-8 max-w-[150px] text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
           Pocket borealis
         </div>
       </div>
-      <div className="relative group h-[450px]">
+      <div className="relative group h-40 lg:h-[450px]">
         <div
-          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/desktop/image-curiosity.jpg')] bg-cover h-[450px] flex flex-col justify-end cursor-pointer`}
+          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/mobile/image-curiosity.jpg')] lg:bg-[url('../assets/images/desktop/image-curiosity.jpg')] bg-cover h-40 lg:h-[450px] flex flex-col justify-end cursor-pointer`}
         ></div>
-        <div className="relative uppercase px-10 py-8 text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
+        <div className="relative uppercase px-10 py-8 max-w-[150px] text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
           The curiosity
         </div>
       </div>
-      <div className="relative group h-[450px]">
+      <div className="relative group h-40 lg:h-[450px]">
         <div
-          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/desktop/image-fisheye.jpg')] bg-cover h-[450px] flex flex-col justify-end cursor-pointer`}
+          className={`group-hover:opacity-40 after:content-[''] after:relative after:min-h-full after:min-w-full after:bg-gradient-to-t after:from-black/30 bg-[url('../assets/images/mobile/image-fisheye.jpg')] lg:bg-[url('../assets/images/desktop/image-fisheye.jpg')] bg-cover h-40 lg:h-[450px] flex flex-col justify-end cursor-pointer`}
         ></div>
-        <div className="relative uppercase px-10 py-8 text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
+        <div className="relative uppercase px-10 py-8 max-w-[250px] text-3xl text-white group-hover:text-black -top-32 cursor-pointer">
           Make it fisheye
         </div>
       </div>
--- src/components/HomeBot.tsx
+++ src/components/HomeBot.tsx
@@ -7,10 +7,10 @@ export const HomeBot = () => {
   return (
     <div>
       <div className="flex justify-between">
-        <div className={`${font.className} text-5xl uppercase`}>
+        <div className={`${font.className} text-3xl md:text-5xl text-center md:text-left w-full md:w-auto uppercase`}>
           Our creations
         </div>
-        <div>
+        <div className="hidden md:block">
           <a
             href="#"
             className="border-2 border-black hover:text-white hover:bg-black px-9 py-2 uppercase tracking-[4px]"
@@ -19,9 +19,18 @@ export const HomeBot = () => {
           </a>
         </div>
       </div>
-      <div className="flex justify-center mt-16 mb-52">
+      <div className="flex flex-col items-center justify-center mt-16 mb-28 md:mb-52">
         <Gallery />
+        <div className="flex justify-center md:hidden mt-14">
+          <a
+            href="#"
+            className="border-2 border-black hover:text-white hover:bg-black px-9 py-2 uppercase tracking-[4px]"
+          >
+            See all
+          </a>
+        </div>
       </div>
+      
     </div>
   );
 };
