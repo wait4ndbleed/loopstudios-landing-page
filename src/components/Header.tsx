5a96c664d70c9d08eba30e20ade38d6e215d4aba
wait4ndbleed <73363131+wait4ndbleed@users.noreply.github.com>
2023-10-06 22:43:04 -0300

    fix: update css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,64 +1,25 @@
 import { Josefin_Sans } from "next/font/google";
 
-const font = Josefin_Sans({ subsets: ["latin"], weight: ["300"] });
+const font = Josefin_Sans({ subsets: ['latin'], weight: ['300'] });
 
 export const Header = () => {
   return (
     <header className="max-w-[var(--max-width)] m-auto pt-16 pb-36 pr-[var(--side-padding)] pl-[var(--side-padding)] bg-[url(../assets/images/desktop/image-hero.jpg)] bg-cover bg-no-repeat">
       <div className="flex justify-between">
         <div>
-          <a href="#" aria-label="Home Page">
-            <img src="../assets/images/logo.svg" alt="" />
-          </a>
+          <a href="#" aria-label="Home Page"><img src="../assets/images/logo.svg" alt="" /></a>
         </div>
-        <nav className="text-white font-normal">
-          <ul className="flex gap-10">
-            <li>
-              <a
-                href="#"
-                className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white"
-              >
-                Careers
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white"
-              >
-                Events
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white"
-              >
-                Products
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white"
-              >
-                Support
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <div className="text-white font-normal">
+            <ul className="flex gap-10">
+                <li><a href="#" className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white">About</a></li>
+                <li><a href="#" className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white">Careers</a></li>
+                <li><a href="#" className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white">Events</a></li>
+                <li><a href="#" className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white">Products</a></li>
+                <li><a href="#" className="relative after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2 after:top-[150%] after:h-[3px] after:w-1/2 hover:after:bg-white">Support</a></li>
+            </ul>
+        </div>
       </div>
-      <div
-        className={`${font.className} uppercase text-white text-7xl border-2 border-white w-[550px] p-8 mt-32`}
-      >
+      <div className={`${font.className} uppercase text-white text-7xl border-2 border-white w-[580px] p-8 pr-0 mt-32`}>
         Immersive experiences that deliver
       </div>
     </header>
